c04cbda754a2e605265e61df792b2c8c7d30e5b5
Parth Iyer <pi@ghostlypi.com>
2025-05-20 19:39:52 -0700

    Now with Name Map

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,20 @@ package main;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        HashMap<String, Station> stations = ParseCSV.parse_stations();
-        System.out.println(stations.keySet().size());
+    public static HashMap<String, Station> stations = new HashMap<>();
 
+    public static void init() {
+        ParseCSV.parse_nameMap(Paths.get("resources/names.map"));
+        stations = ParseCSV.parse_stations();
+        System.out.println(stations.size());
+    }
+
+    public static void main(String[] args) {
+        init();
     }
 }
